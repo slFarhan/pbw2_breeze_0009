31ff44812cb4d5ac50ca416a9c831d0b7c63a542
slFarhan <farhansolih0@gmail.com>
2024-10-17 16:37:55 +0700

    modul6

---
+++ resources/views/tampilan-guest.blade.php
@@ -0,0 +1,5 @@
+<x-guest-layout>
+<div class="mb-4 text-sm text-gray-600">
+                    {{ __("anda harus login terlebih dahulu untuk masuk ke halaman ini!") }}
+                </div>
+</x-guest-layout>
